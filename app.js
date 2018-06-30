27f661a2474adff32f7e75f64d84c185627bbe2a
jodymbrooks <jody@jodymbrooks.com>
2018-06-29 23:38:39 -0500

    Committing from develop after jody branch merged in

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+const favicon = require('serve-favicon'); // eslint-disable-line
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({
+  secret: 'onomatopoeia',
+  resave: false,
+  saveUninitialized: false,
+});
+const User = require('./models/user');
 
 const index = require('./routes/index');
 const api = require('./routes/api/index');
+const users = require('./routes/api/users');
 
 const app = express();
+
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/musiclist');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.user(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.user(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/api/users', users);
+
+// Configure Passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
